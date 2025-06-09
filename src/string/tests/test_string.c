1b5ad024deb059e1bbb49a494aa1651d8bc4a514
Scull <alexscull@D-23303063.localdomain>
2025-06-09 21:28:00 +0300

    Добавление требований к выводу тестов

--- src/string/tests/test_string.c
+++ src/string/tests/test_string.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main() {
-  printf("FAIL: бла-бла\n");
-  printf("PASS: бла-бла\n");
 
-  return 1;
+int main() {
+  /*Требования к выводу тестов:
+
+  Формат сообщений:
+
+  Каждая проверка должна выводить результат в формате:
+
+  text
+  [PASS|FAIL]: <описание проверки>
+
+  Пример:
+
+  text
+  PASS: memchr test 1 - basic functionality
+  FAIL: memcmp test 3 - compare with null pointer
+
+  Коды возврата:
+
+  Тест должен возвращать:
+
+  0 (успех) - если все проверки в тесте пройдены
+
+  1 (ошибка) - если хотя бы одна проверка не пройдена*/
 }
