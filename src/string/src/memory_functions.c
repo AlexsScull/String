04ea2b68848b9ea84cc49e0a5234c7c622c902d7
Yoda Lorinda <yodalori@student.21-school.ru>
2025-07-06 14:03:08 +0300

    добавлена memcmp

--- src/string/src/memory_functions.c
+++ src/string/src/memory_functions.c
@@ -24,6 +24,7 @@ void *s21_memchr(const void *str, int c, s21_size_t n) {
     }
     return NULL;
 }
+
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
   /* Задача:
 Сравнить первые n байтов памяти str1 и str2.
@@ -40,7 +41,21 @@ int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
 > 0, если первый несовпадающий байт в str1 больше.
 
 Если n = 0, возвращает 0.*/
+	const unsigned char *ptr_str_1 = (const unsigned char *)str1;
+	const unsigned char *ptr_str_2 = (const unsigned char *)str2;
+
+	for(size_t i = 0; i < n; i++){
+		int res = ptr_str_1[i] - ptr_str_2[i];
+		if(res != 0){
+			return res;
+		}
+	}
+
+	return 0;
+
 }
+
+
 void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
   /* Задача:
 Скопировать n байтов из src в dest.
